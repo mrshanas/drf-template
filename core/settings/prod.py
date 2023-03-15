b0cce8f2427f9381f89791e9498b438c9cb1779f
Shanas <nassibshaban345@gmail.com>
2023-03-15 18:49:19 +0300

    prod.py

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -3,15 +3,13 @@ from .base import *
 DEBUG = False
 
 # In production, the browsable API is disabled using the renderer class
-REST_FRAMWORK = {
+REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),  
-
-
 }
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
