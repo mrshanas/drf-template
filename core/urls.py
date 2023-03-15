7a4c84980e54d3f13d1a00484666db0d7a9cf742
Shanas <nassibshaban345@gmail.com>
2023-03-15 18:24:26 +0300

    update urls.py

--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
+    path('api/', include('users.urls')),
 
     # docs
     path('', schema_view.with_ui(
