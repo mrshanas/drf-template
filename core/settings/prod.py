f956a688d2c7c9040240891aa28354d80fecf2d5
Shanas <nassibshaban345@gmail.com>
2023-03-15 18:35:13 +0300

    disable browsable api

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -3,7 +3,16 @@ from .base import *
 DEBUG = False
 
 # In production, the browsable API is disabled using the renderer class
-REST_FRAMWORK = {}
+REST_FRAMWORK = {
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ),  
+
+
+}
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
@@ -17,3 +26,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
+# enabling https in production
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
